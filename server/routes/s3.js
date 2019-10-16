d595e52eac634c9527319bd1a37e97e4b9b2f8b0
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-16 16:42:55 -0700

    changed the s3.js so it'll load the stuff

--- server/routes/s3.js
+++ server/routes/s3.js
@@ -4,38 +4,34 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-const BUCKET = 'jvanbucket'
-const REGION = 'us-west-1'
-const ACCESS_KEY = 'AKIAIWPEYRAJOWQNFYCA'
-const SECRET_KEY = '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'
+const BUCKET = 'jvanbucket';
+const REGION = 'us-west-2';
+const ACCESS_KEY = 'AKIAJ6ZAUPT4VX4AUQEQ';
+const SECRET_KEY = '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';
+  try{
+      AWS.config.setPromisesDependency();
+      AWS.config.update({
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_KEY,
+        region: REGION
+      });
+      var s3 = new AWS.S3();
+      s3.listObjectsV2({
+        Bucket: BUCKET
+      },function (err, data) {
+        //handle error
+        if (err) {
+          console.log("Error", err);
+        }
+        //success
+        if (data) {
+          console.log("Uploaded in:", data);
+        }});
 
-const localImage = '/yunpreset1.png'
-const imageRemoteName = `yunImage_${new Date().getTime()}.png`
-
-AWS.config.update({
-  accessKeyId: ACCESS_KEY,
-  secretAccessKey: SECRET_KEY,
-  region: REGION
-})
-
-var s3 = new AWS.S3()
-
-s3.putObject({
-  Bucket: BUCKET,
-  Body: fs.readFileSync(__dirname +localImage),
-  Key: imageRemoteName
-})
-  .promise()
-  .then(response => {
-    console.log(`done! - `, response)
-    res.send(
-      `The URL is ${s3.getSignedUrl('getObject', { Bucket: BUCKET, Key: imageRemoteName })}`
-    )
-  })
-  .catch(err => {
-    console.log('failed:', err)
-  })
+      }
+  catch(e){
+      console.log("our error" + e);
+  }
 });
 
 module.exports = router;
-
